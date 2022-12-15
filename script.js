c1b57b98441d80b758e43f449cab18b6818c3d35
zarahatsagortsyan <69346017+zarahatsagortsyan@users.noreply.github.com>
2022-12-15 12:48:01 +0400

    Add files via upload
    Web Site for individual work of WEB-technologies EUA
    student` Zaruhi Hatsagortsyan

---
+++ script.js
@@ -0,0 +1,28 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+   burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("is-active");
+      navbarMenu.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Click Menu Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      burgerMenu.classList.remove("is-active");
+      navbarMenu.classList.remove("is-active");
+   });
+});
+
+// Change Header Background on Scrolling
+window.addEventListener("scroll", () => {
+   if (this.scrollY >= 85) {
+      headerMenu.classList.add("on-scroll");
+   } else {
+      headerMenu.classList.remove("on-scroll");
+   }
+});
